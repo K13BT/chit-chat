ae3e8991eea71ae69ccc01d3c5fb4817428d0b7a
Kajal Bhatt <k13bhatt@gmail.com>
2024-04-14 02:36:57 +0530

    removed log

--- client/src/socket.js
+++ client/src/socket.js
@@ -5,8 +5,6 @@ const URL =
     ? "https://chit-chat-c4d0.onrender.com/"
     : "http://localhost:8000";
 
-console.log(process.env.REACT_APP_NODE_ENV);
-
 export const socket = io(URL, {
   autoConnect: false,
 });
