3f91eadda1afede60072ff344f41dbf6cb769f4e
Kajal Bhatt <k13bhatt@gmail.com>
2024-04-14 02:25:07 +0530

    added url

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const URL =
   process.env.NODE_ENV === "production"
-    ? process.env.URL
+    ? "https://chit-chat-c4d0.onrender.com/"
     : "http://localhost:8000";
 
 export const socket = io(URL, {
